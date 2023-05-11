acfcdc3412a069680bb423a2059fa365e2ea2ff7
eveperc <eve.perc@icloud.com>
2023-05-11 18:20:41 +0900

    add Nuxt.js

---
+++ frontend/app.vue
@@ -0,0 +1,25 @@
+<script>
+import { HelloRequest } from './hello_pb';
+import GreetingService from './hello_connectweb';
+
+// GreetingServiceを使ってサーバーと通信する
+const service = GreetingService;
+// リクエストを作成する
+const request = new HelloRequest();
+request.name = 'Vue.js';
+
+//ボタンがクリックされたらconsole.log
+export default {
+  methods: {
+    handleClick() {
+      console.log(true);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <button @click="handleClick">Click me</button>
+  </div>
+</template>
